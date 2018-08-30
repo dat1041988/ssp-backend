b5798abc785ce549f5aa9d87e65a400608d80f08
dat1041988 <42293819+dat1041988@users.noreply.github.com>
2018-08-30 15:39:39 +0700

    Update quotas.go

--- server/openshift/quotas.go
+++ server/openshift/quotas.go
@@ -13,7 +13,7 @@ import (
 	"strings"
 
 	"github.com/Jeffail/gabs"
-	"github.com/SchweizerischeBundesbahnen/ssp-backend/server/common"
+	"github.com/dat1041988/ssp-backend/server/common"
 	"github.com/gin-gonic/gin"
 )
 
